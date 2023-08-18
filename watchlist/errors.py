739338ea5c5163aef6fc61f8abc9d02a31f56e8c
Milton Chang <mchangpi@gmail.com>
2023-08-18 19:54:12 +0800

    C10-2 Organize application with package coverage run --source=watchlist test_watchlist.py

--- watchlist/errors.py
+++ watchlist/errors.py
@@ -8,7 +8,7 @@ def bad_request(e):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
